c41ea49a485924bf480a154e9da04c649b504490
DareAnanas <nylo2005@gmail.com>
2024-11-23 17:29:06 +0200

    process env

--- server/index.cjs
+++ server/index.cjs
@@ -14,7 +14,7 @@ const db = mysql.createConnection({
     host: 'junction.proxy.rlwy.net', // Replace with your MariaDB host
     port: 32806,
     user: 'root', // Replace with your MariaDB user
-    password: ${{MariaDB.MARIADB_ROOT_PASSWORD}}, // Replace with your MariaDB password
+    password: process.env.MARIADB_ROOT_PASSWORD, // Replace with your MariaDB password
     database: 'railway', // Replace with your MariaDB database
 });
 
